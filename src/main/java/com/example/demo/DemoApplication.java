56d68909695a42040b583eaca588d81bdeba97ee
Feroz <feroz112597@gmail.com>
2023-12-06 17:45:28 +0530

    browsercheck

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication implements CommandLineRunner {
 
     @Value("${spring.application.name}")
@@ -20,8 +31,32 @@ public class DemoApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Display the content of application.properties
+        // Display the content in the console
         System.out.println("Application Name: " + appName);
         System.out.println("Server Port: " + serverPort);
+    }
+
+    @GetMapping("/")
+    public String displayProperties(Model model) {
+        // Read properties from the application.properties file
+        Properties properties = readPropertiesFromFile("src/main/resources/application.properties");
+
+        // Add properties to the model
+        Map<String, String> propertiesMap = new HashMap<>();
+        properties.forEach((key, value) -> propertiesMap.put(key.toString(), value.toString()));
+        model.addAttribute("properties", propertiesMap);
+
+        // Return the template name
+        return "properties";
+    }
+
+    private Properties readPropertiesFromFile(String fileName) {
+        Properties properties = new Properties();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            properties.load(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
     }
 }
